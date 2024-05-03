fb0dcf03d34500f55c43a62907bd6da7650e61d8
Eleni Neti <dit2218dsc@go.uop.gr>
2024-05-03 16:45:11 +0300

    Exercise #20

---
+++ 99_bottles.py
@@ -0,0 +1,5 @@
+
+# range(start, end, step) for reverse counting step = -1
+
+for i in range(99, 0, -1):
+  print(f"{i} bottles of beer on the wall \n{i} bottles of beer \nTake one down, pass it around \n{i-1} bottles of beer on the wall")
