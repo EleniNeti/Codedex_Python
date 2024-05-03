83133208011f0fa954900c120e4e9f589cb64689
Eleni Neti <dit2218dsc@go.uop.gr>
2024-05-03 17:23:00 +0300

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,9 +1,13 @@
+# Funny little game "Guess the number". 
+# Our number is 6.
+# The user has 5 tries to guess correctly the number.
+
 guess = 0
 tries = 0
 
 while guess != 6 and tries <= 4:
   tries = tries + 1
-  guess = int(input("Guess the number: "))
+  guess = int(input("Our number lies between 1 and 10. You have 5 tries.\n Guess the number: "))
 
 if tries == 5:
   print("You have reached your maximum number of trials.")
